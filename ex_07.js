c48f33e175e8726aa61c752377c8cedfa269d6bb
bilal <bilal.malem@epitech.digital>
2025-04-27 13:29:10 +0200

    deleted ex 7

--- ex_07.js
+++
@@ -1,7 +0,0 @@
-fruits = ["Blackberries", "Apple", "Banana", "Mango", "Peaches", "Kiwi"];
-function getFruitNameFromIndex(index) {
-    let fruit = "Banana"
-    let index =
-}
-let result = getFruitNameFromIndex("last")
-displayResult(result)
